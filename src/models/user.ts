31e6b131a55e0796e8c1d3f84d00c89eb90551fb
derickfan <fanderick9999@gmail.com>
2021-07-26 22:59:17 -0400

    create User model

---
+++ src/models/user.ts
@@ -0,0 +1,53 @@
+import { Column, CreatedAt, Table } from "sequelize-typescript";
+import { DataTypes, Model, Optional } from "sequelize";
+import sequelize from "sequelize";
+import { UUIDV4 } from "sequelize";
+
+interface UserAttributes {
+  id: string,
+  username: string,
+  email: string,
+  password: string,
+  profilePhotoUrl: string,
+}
+
+interface UserCreationAttributes extends Optional<UserAttributes, "id">{}
+
+module.exports = (sequelize: any, DataTypes: any) => {
+  class User extends Model<UserAttributes> implements UserAttributes {
+    email!: string;
+    password!: string;
+    id!: string;
+    username!: string;
+    profilePhotoUrl!: string;
+  }
+  User.init({
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: UUIDV4,
+      allowNull: false,
+      primaryKey: true
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    profilePhotoUrl: {
+      type: DataTypes.STRING,
+    }
+  }, {
+    sequelize,
+    modelName: "User"
+  });
+
+  return User;
+}
