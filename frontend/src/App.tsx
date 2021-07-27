1ff3e04a32879ff5bbf4a421e1011d2860030bba
derickfan <fanderick9999@gmail.com>
2021-07-26 23:01:49 -0400

    Add styled components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,30 @@
+import styled from "styled-components";
 import React from 'react';
+import LoginForm from "./components/LoginForm";
+
 function App() {
   return (
-    <div></div>
+    <Container>
+      <Title>ChitChat</Title>
+      <LoginForm />
+    </Container>
   );
 }
 
 export default App;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100vw;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  background-color: #31313D;
+`;
+
+const Title = styled.h1`
+  font-family: 'Chewy', cursive;
+  padding: 1rem;
+  font-size: 5rem;
+`
