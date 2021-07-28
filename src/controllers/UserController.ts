ed24542da2edba7ad1c0f15200c48be8da9a473a
derickfan <fanderick9999@gmail.com>
2021-07-27 23:56:28 -0400

    implement createUser function

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,9 +1,45 @@
+const db = require("../models");
+
+interface UserData {
+  username: string;
+  password: string;
+  email: string;
+  profilePhotoUrl?: string;
+}
+
+const { User } = db;
 class UserController {
   
   defaultMethod() {
     return {
       text: "Hello world"
     };
+  }
+
+  async createUser(data: UserData) {
+    try {
+      const user = await User.create(data);
+      return user;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  
+  async getUser() {
+    try {
+      const users = await User.findAll();
+      return users;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async updateUser() {
+
+  }
+
+  async deleteUser() {
+
   }
 
 }
