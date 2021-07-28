ddff4e570dfad25dc2600ecfd4c8a60b3b426a9f
derickfan <fanderick9999@gmail.com>
2021-07-27 23:53:08 -0400

    add new user routes

--- src/router/UserRouter.ts
+++ src/router/UserRouter.ts
@@ -2,23 +2,36 @@ import { Router } from "express";
 import UserController from "../controllers/UserController";
 
 class UserRouter {
-  private _router = Router();
-  private _controller = UserController;
+	private _router = Router();
+	private _controller = UserController;
 
-  get router() {
-    return this._router;
-  }
+	get router() {
+		return this._router;
+	}
 
-  constructor() {
-    this._configure();
-  }
+	constructor() {
+		this._configure();
+	}
 
-  private _configure() {
-    this._router.get('/', (_, res) => {
-      res.status(200).json(this._controller.defaultMethod());
-    })
-  }
+	private _configure() {
+		this._router.get("/", async (_, res) => {
+      const users = await this._controller.getUser();
+			res.status(200).json(users);
+		});
 
+		this._router.post("/login", async (req, res) => {
+			res.send("Login Successful");
+		});
+
+		this._router.post("/logout", (req, res) => {
+			res.send("Logout Successful");
+		});
+
+		this._router.post("/signup", (req, res) => {
+			const result = this._controller.createUser(req.body);
+      res.send(result);
+		});
+	}
 }
 
 export = new UserRouter().router;
