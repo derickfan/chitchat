94c13cc338f5ab45bbf526948ec1d8cf6ace59d1
derickfan <fanderick9999@gmail.com>
2021-07-27 23:53:27 -0400

    add cors to express

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import express from "express";
+import cors from "cors";
 import Logger from "./middleware/Logger";
 import MainRouter from "./router/MainRouter";
 
@@ -16,7 +17,13 @@ const PORT = process.env.APP_PORT || 5000;
 const app = express();
 const router = MainRouter;
 
+const options: cors.CorsOptions = {
+  origin: "http://localhost:3000"
+};
+
 app.use(Logger);
+app.use(cors(options));
+app.use(express.json());
 app.use("/api", router);
 
 app.listen(PORT, () => console.log(`Server is running on PORT:${PORT}`));
