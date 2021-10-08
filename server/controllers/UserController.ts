3ed8eda5912d1a1f7d7babd78be5fae0307dbc82
derickfan <fanderick9999@gmail.com>
2021-10-08 13:29:39 -0400

    implement basic User CRUD functionality

---
+++ server/controllers/UserController.ts
@@ -0,0 +1,78 @@
+import User from "../models/User";
+
+interface UserData {
+	username: string;
+	password: string;
+	email: string;
+	profilePhotoUrl: string;
+}
+
+/**
+ *
+ * @param data - js object used to create new user
+ * @returns a newly creted user wrapped in a Promise
+ * @throws a generic Error
+ */
+export const createUser = async (data: UserData): Promise<User> => {
+	try {
+		const user = await User.create(data);
+		return user;
+	} catch (error) {
+		throw new Error(error);
+	}
+};
+
+/**
+ * @returns a array of Users in the database
+ */
+export const getAllUsers = async (): Promise<User[]> => {
+	const users = await User.findAll();
+	return users;
+};
+
+/**
+ * 
+ * @param username - the name of the user that is being updated
+ * @param key - the field that is being updated
+ * @param value - the new value that the field is being updated to
+ * @returns a updated User
+ * @throws a generic Error
+ */
+export const updateUser = async (
+	username: string,
+	key: keyof UserData,
+	value: string
+): Promise<User> => {
+	try {
+		const user = await User.findOne({
+			where: {
+				username: username,
+			},
+		});
+		user.update(key, value);
+		return user;
+	} catch (error) {
+		throw new Error(error);
+	}
+};
+
+/**
+ * 
+ * @param username - the name of the user thats being deleted
+ * @param password - the password of the user to confirm deletion
+ * @returns nothing
+ * @throws a generic Error
+ */
+export const deleteUser = async ( username: string, password: string ): Promise<void> => {
+	try {
+		const user = await User.findOne({
+			where: {
+				username: username,
+				password: password,
+			}
+		});
+		await user.destroy();
+	} catch (error) {
+		throw new Error(error);
+	}
+};
