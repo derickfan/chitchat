7b40d2a6fe249dcff5333cf6a5cbcf3742239084
derickfan <fanderick9999@gmail.com>
2021-10-24 01:05:39 -0400

    add MUI dark themes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,34 @@
-import React from 'react';
-import './App.css';
-import { UserProvider } from './hooks/UserContext';
-import RouterPage from './pages/RouterPage';
+import React from "react";
+import "./App.css";
+import { UserProvider } from "./hooks/UserContext";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import RouterPage from "./pages/RouterPage";
+import { CssBaseline } from "@mui/material";
 
 function App() {
-  return (
-    <UserProvider>
-			<RouterPage />
-		</UserProvider>
-  );
+	const theme = createTheme({
+		palette: {
+			mode: "dark",
+		},
+		components: {
+			MuiOutlinedInput: {
+				styleOverrides: {
+					input: {
+						"background-color": "black"
+					}
+				},
+			},
+		},
+	});
+
+	return (
+		<ThemeProvider theme={theme}>
+			<CssBaseline />
+			<UserProvider>
+				<RouterPage />
+			</UserProvider>
+		</ThemeProvider>
+	);
 }
 
 export default App;
