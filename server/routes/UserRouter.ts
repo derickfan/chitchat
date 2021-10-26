0f4215b3a10c99da378ed01928a6f71ed93a23d2
derickfan <fanderick9999@gmail.com>
2021-10-25 23:23:14 -0400

    add auth and session handling

--- server/routes/UserRouter.ts
+++ server/routes/UserRouter.ts
@@ -1,10 +1,28 @@
 import { Router } from "express";
 import * as UserController from "../controllers/UserController";
+import passport from "../middlewares/authentication";
 
 const router = Router();
 
 router.get("/", (req, res) => {
 	res.status(200).json({ data: "User Router Online" });
+});
+
+router.post("/login", passport.authenticate("local"), (req, res) => {
+	try {
+		res.json(req.user);
+	} catch (err) {
+		res.status(401).json({ error: err });
+	}
+});
+
+router.post("/logout", (req, res) => {
+	try {
+		req.logout();
+		res.status(200).json("Successfully logged out");
+	} catch (error) {
+		res.status(404).json({ error });
+	}
 });
 
 router.post("/signup", async (req, res, next) => {
@@ -14,6 +32,14 @@ router.post("/signup", async (req, res, next) => {
 	} catch (error) {
 		next(error);
 	} 
+});
+
+router.get("/check", (req, res) => {
+	if (req.user) {
+		res.status(200).json(req.user);
+	} else {
+		res.status(400).json({ error: "Not authenticated" });
+	}
 });
 
 export default router;
--- server/server.ts
+++ server/server.ts
@@ -7,6 +7,8 @@ import User from "./models/User";
 import Conversation from "./models/Conversation";
 import Message from "./models/Message";
 import router from "./routes";
+import expressSession from "express-session";
+import passport from "./middlewares/authentication";
 
 db.sync().then(() => {
 	console.log("Connected to the database");
@@ -15,7 +17,6 @@ db.sync().then(() => {
 User.sync();
 Conversation.sync();
 Message.sync();
-
 
 const PORT = process.env.PORT || 5000;
 
@@ -28,6 +29,21 @@ const io = new Server(http, {
 		origin: "http://localhost:3000",
 	},
 });
+
+app.use(
+	expressSession({
+		secret: "SECRET",
+		resave: false,
+		saveUninitialized: false,
+		cookie: {
+			secure: false,
+			maxAge: 1000 * 60 * 60,
+		},
+	})
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(express.static("../client/build"));
 
