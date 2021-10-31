e00919e4d75e55baa7744a44d1152be26de3218a
derickfan <fanderick9999@gmail.com>
2021-10-31 04:24:13 -0400

    add new themes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,7 +5,7 @@ import { createTheme, ThemeProvider } from "@mui/material/styles";
 import RouterPage from "./pages/RouterPage";
 import { Button, CssBaseline, styled } from "@mui/material";
 import { ThemeContext } from "./hooks/ThemeContext";
-import { green } from "@mui/material/colors";
+import { amber, blue, green, orange, yellow } from "@mui/material/colors";
 
 function App() {
 	const { darkMode, toggleDarkMode } = useContext(ThemeContext);
@@ -15,6 +15,23 @@ function App() {
 			// Applying this to a Typography does not work???
 			success: { main: green[500] },
 			mode: darkMode ? "dark" : "light",
+			...(darkMode
+				? {
+						primary: {
+							main: "#bf360c",
+						},
+						secondary: {
+							main: "#ff6f00",
+						},
+				  }
+				: {
+					primary: {
+						main: '#00bfa5',
+					},
+					secondary: {
+						main: '#a7ffeb',
+					},
+				  }),
 		},
 		components: {
 			MuiOutlinedInput: {
