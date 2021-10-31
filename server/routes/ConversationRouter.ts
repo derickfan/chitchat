140f8b855274b8c7f6dcee68af7caa406ec66297
derickfan <fanderick9999@gmail.com>
2021-10-31 11:49:43 -0400

    add /user/create & /user/getUser

--- server/routes/ConversationRouter.ts
+++ server/routes/ConversationRouter.ts
@@ -16,4 +16,22 @@ router.get("/all", async (req, res, next) => {
     }
 });
 
+router.post("/create", async (req, res, next) => {
+    try {
+        const conversation = await ConversationController.createConveration(req.body);
+        res.status(201).json({ data: conversation });
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.get("/getUser", async (req, res, next) => {
+    try {
+        const conversations = await ConversationController.getUserConversations(req.body.userId);
+        res.status(200).json({ data: conversations });
+    } catch (error) {
+        next(error);
+    }
+})
+
 export default router;
