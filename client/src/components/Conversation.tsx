8cfa73beaca3ad8a91881ccb9517b1d51aa18132
derickfan <fanderick9999@gmail.com>
2021-11-13 11:16:53 -0500

    formating

--- client/src/components/Conversation.tsx
+++ client/src/components/Conversation.tsx
@@ -14,17 +14,17 @@ interface IProps {
 const Conversation = (props: IProps) => {
 	const theme = useTheme();
 	const { user } = useContext(UserContext);
-	const [ text, setText ] = useState<string>("");
+	const [text, setText] = useState<string>("");
 	const { selectedConversation, sendMessage } = props;
 	const typingIndicatorRef = createRef<HTMLDivElement>();
 
 	useEffect(() => {
-		scrollIntoView()
+		scrollIntoView();
 	}, [selectedConversation]);
 
 	const scrollIntoView = () => {
-		typingIndicatorRef.current?.scrollIntoView({behavior: "auto"});
-	}
+		typingIndicatorRef.current?.scrollIntoView({ behavior: "auto" });
+	};
 
 	const onEnter = () => {
 		if (!user) throw new Error("User not logged in");
@@ -33,13 +33,12 @@ const Conversation = (props: IProps) => {
 			content: text,
 			converstaionId: selectedConversation.id,
 			createdAt: new Date(),
-			username: user?.username
-		}
+			username: user?.username,
+		};
 		sendMessage(message);
 		setText("");
 		// scrollIntoView();
-	}
-
+	};
 
 	return (
 		<FlexContainer
@@ -49,63 +48,57 @@ const Conversation = (props: IProps) => {
 			padding="1rem 0rem"
 			width="100%"
 		>
-			{!selectedConversation ? (
-				<h1>Select a conversation or create a new one</h1>
-			) : (
-				<>
-					<h1>{selectedConversation.name}</h1>
-					<MessageContainer
-						height="100%"
-						align="flex-start"
+			<h1>{selectedConversation.name}</h1>
+			<MessageContainer
+				height="100%"
+				align="flex-start"
+				justify="flex-start"
+			>
+				{selectedConversation.messages.map((e) => (
+					<FlexContainer
+						direction={
+							e.username === user?.username
+								? "row-reverse"
+								: "row"
+						}
+						height="auto"
+						width="100%"
+						margin="0.5rem 0"
+						// padding="0.5rem"
+						// width="500px"
+						align="flex-end"
 						justify="flex-start"
 					>
-						{selectedConversation.messages.map((e) => (
-							<FlexContainer
-								direction={
-									e.username === user?.username
-										? "row-reverse"
-										: "row"
-								}
-								height="auto"
-								width="100%"
-								margin="0.5rem 0"
-								// padding="0.5rem"
-								// width="500px"
-								align="flex-end"
-								justify="flex-start"
-							>
-								<ProfilePicture src="https://cdn.discordapp.com/attachments/792881224753872929/875159970444894218/image_2.png" />
-								<MessageBubble
-									color={
-										e.username === user?.username
-											? theme.palette.secondary.main
-											: theme.palette.primary.main
-									}
-									textAlign="left"
-									width="fit-content"
-									height="fit-content"
-									padding="0.5rem 1rem"
-									margin="0 1rem"
-								>
-									{e.content}
-								</MessageBubble>
-							</FlexContainer>
-						))}
-						<div ref={typingIndicatorRef}></div>
-					</MessageContainer>
-					<FlexContainer direction="row" height="auto">
-						<TextField
-							size="small"
-							placeholder="Enter your message..."
-							onChange={(e) => setText(e.target.value)}
-							value={text}
-							multiline
-							fullWidth
-						/>
-						<Button onClick={onEnter}>Send</Button>
+						<ProfilePicture src="https://cdn.discordapp.com/attachments/792881224753872929/875159970444894218/image_2.png" />
+						<MessageBubble
+							color={
+								e.username === user?.username
+									? theme.palette.secondary.main
+									: theme.palette.primary.main
+							}
+							textAlign="left"
+							width="fit-content"
+							height="fit-content"
+							padding="0.5rem 1rem"
+							margin="0 1rem"
+						>
+							{e.content}
+						</MessageBubble>
 					</FlexContainer>
-				</>
-			)}
+				))}
+				<div ref={typingIndicatorRef}></div>
+			</MessageContainer>
+			<FlexContainer direction="row" height="auto">
+				<TextField
+					size="small"
+					placeholder="Enter your message..."
+					onChange={(e) => setText(e.target.value)}
+					value={text}
+					multiline
+					fullWidth
+				/>
+				<Button onClick={onEnter}>Send</Button>
+			</FlexContainer>
 		</FlexContainer>
 	);
 };
