3f1dbcb84f61b6f2ccd0ebf34ac0b6fa499ec89a
derickfan <fanderick9999@gmail.com>
2021-11-12 23:28:43 -0500

    format

--- server/routes/ConversationRouter.ts
+++ server/routes/ConversationRouter.ts
@@ -4,34 +4,39 @@ import * as ConversationController from "../controllers/ConversationController";
 const router = Router();
 
 router.get("/", (req, res) => {
-    res.status(200).json({ data: "User Router Online" });
+	res.status(200).json({ data: "User Router Online" });
 });
 
 router.get("/all", async (req, res, next) => {
-    try {
-        const conversations = await ConversationController.getAllConversations();
-        res.status(200).json({ data: conversations });
-    } catch (error) {
-        next(error);
-    }
+	try {
+		const conversations =
+			await ConversationController.getAllConversations();
+		res.status(200).json({ data: conversations });
+	} catch (error) {
+		next(error);
+	}
 });
 
 router.post("/create", async (req, res, next) => {
-    try {
-        const conversation = await ConversationController.createConveration(req.body);
-        res.status(201).json({ data: conversation });
-    } catch (error) {
-        next(error);
-    }
+	try {
+		const conversation = await ConversationController.createConveration(
+			req.body
+		);
+		res.status(201).json({ data: conversation });
+	} catch (error) {
+		next(error);
+	}
 });
 
-router.get("/getUser", async (req, res, next) => {
-    try {
-        const conversations = await ConversationController.getUserConversations(req.body.userId);
-        res.status(200).json({ data: conversations });
-    } catch (error) {
-        next(error);
-    }
-})
+router.post("/getUser", async (req, res, next) => {
+	try {
+		const conversations = await ConversationController.getUserConversations(
+			req.body.userId
+		);
+		res.status(200).json({ data: conversations });
+	} catch (error) {
+		next(error);
+	}
+});
 
 export default router;
