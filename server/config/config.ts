91f9a61157272f2c6e205e0bc10e0add62dea89d
derickfan <fanderick9999@gmail.com>
2022-01-25 13:32:57 -0500

    add dotenv

--- server/config/config.ts
+++ server/config/config.ts
@@ -1,13 +1,17 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
 interface DbConfigType {
 	[key: string]: { [key: string]: string };
 }
 
 const dbConfigs: DbConfigType = {
 	"development": {
-		"username": "derickfan",
-		"password": "password",
-		"database": "chitchat-db",
-		"host": "localhost",
+		"username": `${process.env.DB_USERNAME}`,
+		"password": `${process.env.DB_PASSWORD}`,
+		"database": `${process.env.DB_NAME}`,
+		"host": `${process.env.DB_HOST}`,
 		"dialect": "postgres"
 	}
 };
