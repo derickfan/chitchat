29a570571eb1974017bac1e04c4366be26206d0b
derickfan <fanderick9999@gmail.com>
2022-01-27 14:48:19 +0100

    setup jest

--- server/config/config.ts
+++ server/config/config.ts
@@ -1,10 +1,20 @@
 import dotenv from "dotenv";
+import { Dialect } from "sequelize/types";
 
 dotenv.config();
 
 interface DbConfigType {
-	[key: string]: { [key: string]: string };
+	[key: string]: { 
+		"username": string,
+		"password": string,
+		"database": string,
+		"host": string,
+		"logging": boolean,
+		"dialect": Dialect 
+	};
 }
+
+
 
 const dbConfigs: DbConfigType = {
 	"development": {
@@ -12,6 +22,15 @@ const dbConfigs: DbConfigType = {
 		"password": `${process.env.DB_PASSWORD}`,
 		"database": `${process.env.DB_NAME}`,
 		"host": `${process.env.DB_HOST}`,
+		"logging": true,
+		"dialect": "postgres"
+	},
+	"test": {
+		"username": `${process.env.DB_TEST_USERNAME}`,
+		"password": `${process.env.DB_TEST_PASSWORD}`,
+		"database": `${process.env.DB_TEST_NAME}`,
+		"host": `${process.env.DB_TEST_HOST}`,
+		"logging": false,
 		"dialect": "postgres"
 	}
 };
