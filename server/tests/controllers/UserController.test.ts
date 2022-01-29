4f7c8b03b57ffc11d49643a6b521a4fb764464bb
derickfan <fanderick9999@gmail.com>
2022-01-29 15:24:30 +0100

    change tests

--- server/tests/controllers/UserController.test.ts
+++ server/tests/controllers/UserController.test.ts
@@ -5,14 +5,14 @@ describe("test UserController", () => {
 
     let testDB = db;
 
-    beforeAll(async () => {
+    beforeEach(async () => {
         await testDB.sync({ force: true });
-    })
+    });
 
     afterAll(done => {
         testDB.close();
         done();
-    })
+    });
 
     test("createUser returns the newly created user", async () => {
         const user = await UserController.createUser({
@@ -27,15 +27,26 @@ describe("test UserController", () => {
         expect(user.id.length).toEqual(36);
     });
 
-    test("getAllUsers returns an array of 1 User", async () => {
+    test("getAllUsers returns an array of all Users", async () => {
+        await UserController.createUser({
+            email: "testuser@gmail.com",
+            password: "testuser",
+            username: "testuser",
+        });
+
         const users = await UserController.getAllUsers();
         expect(users.length).toEqual(1);
     });
 
     test("getAllOtherUsers returns all the other users", async () => {
         const user = await UserController.createUser({
+            email: "testuser@gmail.com",
+            password: "testuser",
+            username: "testuser",
+        });
+        await UserController.createUser({
             email: "testuser1@gmail.com",
-            password: "testuser1",
+            password: "testuser",
             username: "testuser1",
         });
 
@@ -44,20 +55,36 @@ describe("test UserController", () => {
     });
 
     test("getUser returns the user", async () => {
+        await UserController.createUser({
+            email: "testuser@gmail.com",
+            password: "testuser",
+            username: "testuser",
+        });
+
         const user = await UserController.getUser("testuser");
         expect(user.username).toEqual("testuser");
     });
 
     test("updateUser updates the username", async () => {
-        const user = await UserController.getUser("testuser");
+        await UserController.createUser({
+            email: "testuser@gmail.com",
+            password: "testuser",
+            username: "testuser",
+        });
         
         const updatedUser = await UserController.updateUser("testuser", "username", "updatedUsername");
         expect(updatedUser.username).toEqual("updatedUsername");
     });
 
     test("deleteUser successfully deletes the user", async () => {
-        await UserController.deleteUser("testuser1", "testuser1");
+        await UserController.createUser({
+            email: "testuser@gmail.com",
+            password: "testuser",
+            username: "testuser",
+        });
+
+        await UserController.deleteUser("testuser", "testuser");
         const users = await UserController.getAllUsers();
-        expect(users.length).toEqual(1);
+        expect(users.length).toEqual(0);
     });
 })
